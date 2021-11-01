b503048609f6a1d264cfaee30f10ba65a406a121
Taiki <79706969+ksfw14@users.noreply.github.com>
2021-11-01 22:23:17 +0900

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -22,3 +22,38 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("mask").classList.toggle("active");
     })
 });
+
+
+//文字アニメーション
+const animationTargetElements = document.querySelectorAll(".mainPicFont");
+
+for (let i = 0; i < animationTargetElements.length; i++) {
+    const targetElement = animationTargetElements[i];
+    texts = targetElement.textContent;
+    textsArray = [];
+
+    targetElement.textContent = "";
+
+    /*一旦テキストを配列に代入して中身を全部消す */
+    for (let g = 0; g < texts.split("").length; g++) {
+        if (texts.split("")[g] === " ") {
+            textsArray.push(" ");
+        } else {
+            textsArray.push(
+                '<span style="animation-delay: ' + (g * 0.04) + 's;">' + texts.split("")[g] + "</span>" /*split("")文字を一個一個配列に入れる */
+            );
+        }
+    }
+    /*一個一個の文字を配列にいれる */
+    for (let j = 0; j < textsArray.length; j++) {
+        targetElement.innerHTML += textsArray[j];
+        /*innerHTML: */
+    }
+}
+
+
+//送信ボタンアラート
+
+function clickEvent(){
+    alert('送信されました。')
+}
